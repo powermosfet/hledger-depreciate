25a53ae0d869e14e68a91620fee917937c0ddc0d
Asmund Berge <herrasbe@gmail.com>
2021-05-23 18:56:43 +0200

    Trying even more (nix) stuff

--- default.nix
+++ default.nix
@@ -1,15 +1,3 @@
-let
-  bootstrap = import <nixpkgs> { };
+{ pkgs }:
 
-  nixpkgs = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
-
-  src = bootstrap.fetchFromGitHub {
-    owner = "NixOS";
-    repo  = "nixpkgs";
-    inherit (nixpkgs) rev sha256;
-  };
-
-  pkgs = import src { };
-
-in
-  pkgs.haskellPackages.callPackage ./hledger-depreciate.nix { }
+pkgs.haskellPackages.callPackage ./hledger-depreciate.nix { }
